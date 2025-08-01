80c28d8e8d5c614c225d86f9de7d12646cdf1790
erengaygusuz <gaygusuzeren@gmail.com>
2025-08-01 10:15:14 +0300

    ApiBaseUrl is Updated for Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://localhost:8443/api",
+  apiBaseUrl: "https://api-marketivo.erengaygusuz.com.tr/api",
   stripePublishableKey: "pk_test_51Nvx1SAkCHzlYhgHDLntydENy3WLqtcWq4Rb9LrQbASWWg5BbLdHxAYmRKbvMCzNZEtr7LbRenLgfY6OEcZr6DDS00QFfBKoAk"
 };
