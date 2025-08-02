b731f02662797cacd149a86e6d20c432c84f83a1
erengaygusuz <gaygusuzeren@gmail.com>
2025-08-02 10:21:53 +0300

    New Domains are Added to MyAppConfig

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,14 +3,14 @@ export default {
     domain: 'dev-p2akft8jby3faqa5.us.auth0.com',
     clientId: '4Bt3FnArUUElthacdpLdxSszDTczN1VI',
     authorizationParams: {
-      redirect_uri: 'https://localhost:4200/login/callback',
-      audience: 'http://localhost:8080',
+      redirect_uri: 'https://marketivo.erengaygusuz.com.tr/login/callback',
+      audience: 'https://api-marketivo.erengaygusuz.com.tr',
     },
   },
   httpInterceptor: {
     allowedList: [
-      'http://localhost:8080/api/orders/**',
-      'http://localhost:8080/api/checkout/purchase',
+      'https://api-marketivo.erengaygusuz.com.tr/api/orders/**',
+      'https://api-marketivo.erengaygusuz.com.tr/api/checkout/purchase',
     ],
   },
 };
