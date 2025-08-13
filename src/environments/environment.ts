1f5b173393d24614fb83f38e2147a8486ae9044f
erengaygusuz <gaygusuzeren@gmail.com>
2025-08-13 11:12:13 +0300

    Custom Domain is Added for Auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
     production: true,
-    apiBaseUrl: 'https://marketivo.erengaygusuz.com.tr/api',
+    apiBaseUrl: '/api',
     stripePublishableKey:
         'pk_test_51Nvx1SAkCHzlYhgHDLntydENy3WLqtcWq4Rb9LrQbASWWg5BbLdHxAYmRKbvMCzNZEtr7LbRenLgfY6OEcZr6DDS00QFfBKoAk',
     auth: {
-        domain: 'dev-p2akft8jby3faqa5.us.auth0.com',
+        domain: 'auth.marketivo.erengaygusuz.com.tr',
         clientId: '4Bt3FnArUUElthacdpLdxSszDTczN1VI',
         audience: 'https://marketivo.erengaygusuz.com.tr/api',
     },
